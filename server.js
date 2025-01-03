f991cda9f216afac42101e11ebc47e9449ccd6de
touseefiqbal845 <touseefiqbal845@gmail.com>
2025-01-03 18:37:24 +0500

    host

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use("/api/auth/users", userRoutes);
 app.use("/api/company/data", companyDataRoutes);
 
 // Connect to MongoDB and Start Server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || "https://vristo-two.vercel.app/api";
 
 mongoose
   .connect(process.env.MONGO_URI, {
